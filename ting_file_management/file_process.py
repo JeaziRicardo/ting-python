b791605c71b750b1772cf81d73bac6559122b7c3
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-02-07 22:34:45 -0300

    feat(03): implementa a função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,25 +3,20 @@ import sys
 
 
 def process(path_file, instance):
-    # Verifica se o arquivo já foi processado anteriormente
     if any(item['nome_do_arquivo'] == path_file for item in instance.queue):
         print('[!] O arquivo %s já foi processado anteriormente' % path_file)
         return
 
-    # Chama a função txt_importer para ler o conteúdo do arquivo
     lines = txt_importer(path_file)
 
-    # Monta o dicionário com os dados do arquivo
     data = {
         'nome_do_arquivo': path_file,
         'qtd_linhas': len(lines),
         'linhas_do_arquivo': lines
     }
 
-    # Adiciona os dados no objeto fila
     instance.enqueue(data)
 
-    # Exibe os dados no stdout
     return sys.stdout.write(f"{data}\n")
 
 

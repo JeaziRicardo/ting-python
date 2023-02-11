8fbb2926294c207bc67cb8ed00ca5ef4f2800b4a
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-02-11 10:51:53 -0300

    feat(06): test_basic_priority_queueing com comentários

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,32 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+
+    # Fila
+    pq.enqueue({"qtd_linhas": 10})
+    pq.enqueue({"qtd_linhas": 2})
+    pq.enqueue({"qtd_linhas": 11})
+    pq.enqueue({"qtd_linhas": 3})
+
+    # Alta prioridade
+    assert len(pq.high_priority) == 2
+    # Prioridade regular
+    assert len(pq.regular_priority) == 2
+    # Tamanho total
+    assert len(pq) == 4
+
+    # Verifica o método search
+    assert pq.search(0) == {"qtd_linhas": 2}
+
+    # Verifica o método dequeue
+    assert pq.dequeue() == {"qtd_linhas": 2}
+    assert len(pq) == 3
+
+    # Verifica asserções inválida
+    with pytest.raises(AssertionError):
+        assert pq.search(0) == {"qtd_linhas": 9}
+    with pytest.raises(IndexError):
+        pq.search(5) == {"qtd_linhas": 2}

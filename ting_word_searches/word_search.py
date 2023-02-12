829476bffa40f85f0c4b2f7ff54bfc5e1279940c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-02-12 20:44:25 -0300

    feat(07): implementa a função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,16 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    info = []
+    for item in instance.queue:
+        occs = [{'linha': i + 1}
+                for i, line in enumerate(item['linhas_do_arquivo'])
+                if word.lower() in line.lower()]
+    if len(occs) > 0:
+        info.append({
+            'palavra': word,
+            'arquivo': item['nome_do_arquivo'],
+            'ocorrencias': occs
+        })
+    return info
 
 
 def search_by_word(word, instance):
